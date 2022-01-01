bde08ceba9b7082bb50750a9b2e61b0c55bf4d93
Sherigabia <orionsavvy@gmail.com>
2022-01-01 14:20:34 +0000

    port changed in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const todoControllers = require('./controllers/todoController')
-
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json())
@@ -14,7 +14,7 @@ app.delete('/todos/:todoId', todoControllers.deleteTodoById)
 app.get('/todos/:todoId', todoControllers.getTodoById)
 
 //listen on a port
-app.listen(3000, function () {
+app.listen(PORT, function () {
        console.log(`Server started`);
        mongoose.connect(process.env.LOCAL_DB).then(function(){
         console.log("DB is Connected")
